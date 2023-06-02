1bdd6715cbf138dacaee92e7518f522d663c5f1f
Bernardo Marenco <bernardomarenco@gmail.com>
2023-06-02 09:34:04 -0300

    Updated license notice on un_votes

--- un_votes.py
+++ un_votes.py
@@ -44,15 +44,15 @@ def main(argv=None):
     program_license = '''%s
 
   Created by Bernardo Marenco on %s.
-  Copyright 2023 organization_name. All rights reserved.
+  Copyright (C) 2023 Marcelo Fiori, Federico La Rocca, Bernardo Marenco.
 
-  This file is part of Foobar.
+  This file is part of efficient-ASE.
 
-  Foobar is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+  efficient-ASE is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
-  Foobar is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+  efficient-ASE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License along with Foobar. If not, see <https://www.gnu.org/licenses/>. 
+  You should have received a copy of the GNU General Public License along with efficient-ASE. If not, see <https://www.gnu.org/licenses/>. 
 
 USAGE
 ''' % (program_shortdesc, str(__date__))
